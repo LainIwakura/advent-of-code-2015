db767fc1dfea999398cbea3fd3087c9715ab54cd
LainIwakura <FishLikeHexagon@gmail.com>
2015-12-07 17:14:00 -0500

    Added Day 7

---
+++ Day7/part2.erl
@@ -0,0 +1,41 @@
+-module(part2).
+-export([main/0]).
+
+main() ->
+	ets:new(circuits, [set,public,named_table]),
+	create_circuits(readlines("input")),
+	B = val("a"),
+	create_circuits(readlines("input")),
+	ets:insert(circuits, {"b", B}),
+	io:format("~p~n", [val("a")]).
+
+readlines(Filename) ->
+	{ok, Data} = file:read_file(Filename),
+	[string:tokens(erlang:binary_to_list(Str), "-> ") || 
+	 	Str <- (binary:split(Data, [<<"\n">>], [global]) -- [<<>>])].
+
+create_circuits(Schematics) ->
+	lists:foreach(fun(X) ->
+		{In, Op} = {hd(lists:reverse(X)), lists:droplast(X)},
+		ets:insert(circuits, {In, Op})
+	end, Schematics).
+
+val(X) ->
+	R = case string:to_integer(X) of
+			{error, _} ->
+				case ets:lookup(circuits, X) of
+					[{_, Res}] when is_integer(Res) -> Res;
+					[{_, L}] when is_list(L) -> parse(L)
+				end;
+			{Res, _} -> Res
+	end,
+	ets:insert(circuits, {X, R}),
+	R.
+
+
+parse([X]) -> val(X);
+parse(["NOT", X]) -> bnot val(X);
+parse([X, "AND", Y]) -> val(X) band val(Y);
+parse([X, "OR", Y]) -> val(X) bor val(Y);
+parse([X, "LSHIFT", Y]) -> val(X) bsl val(Y);
+parse([X, "RSHIFT", Y]) -> val(X) bsr val(Y).
